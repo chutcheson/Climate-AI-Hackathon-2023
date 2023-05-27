f35be8d6754c032efaa1464e8add648132191f4e
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 14:46:47 -0700

    some attempts

--- diaita/docs.py
+++ diaita/docs.py
@@ -1,7 +1,27 @@
+from marvin import ai_fn
+
 from diaita.load import load_collection 
 
-def query_document(text):
+def query_document(text, n_results):
     collection = load_collection()
-    docs = collection.query(query_texts=[text], n_results=3)
-    print(docs)
+    res = collection.query(query_texts=[text], n_results=n_results)
+    ids = res['ids'][0]
+    docs = res['documents'][0]
+    return [(idx, doc) for idx, doc in zip(ids, docs)]
+
+def query_summarized_documents(text, n_results):
+    res = []
+    docs = query_document(text, n_results)
+    for idx, doc in docs:
+        res.append({ "citation" : idx, "document" : summarize_document(doc)})
+    return res
+
+@ai_fn
+def summarize_document(document: str) -> str:
+    """
+    You are an expert in USDA organic regulation. You are asked to summarize the requirements for a USDA organic auditor.
+
+    Take the document and summarize it in bullet points that can be used by the auditor.
+    """
+
 
---
+++ diaita/query.py
@@ -0,0 +1,28 @@
+from typing import Dict, List
+
+from marvin import ai_fn
+
+from diaita.docs import query_summarized_docs
+
+def query(query, kind):
+    docs = query_summarized_docs(query)
+    if kind == "auditor":
+        return query_auditor(query, docs)
+    elif kind == "farmer":
+        return query_farmer(query, docs)
+
+@ai_fn
+def query_auditor(question: str, documents: Dict[str, str]) -> str:
+    """
+    You are an expert in USDA organic accreditation and auditing. You are responding to a question from an expert in the field.
+
+    Using the relevant documents, you should answer the question. Include the citations of the documents in your response.
+    """
+
+@ai_fn
+def query_farmer(query: str, documents: Dict[str, str]) -> str:
+    """
+    You are an expert in USDA organic accreditation and auditing. You are responding to a question from a farmer looking to explore USDA organic certification.
+
+    Using the relevant documents, you should answer the question. Include the citations of the documents in your response.
+    """
