a39f8616e4f3da38c62169aa46dd4d581373f692
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 13:01:51 -0700

    fixed df name

--- diaita/load.py
+++ diaita/load.py
@@ -12,7 +12,7 @@ def create_collection():
     directory = os.path.expanduser("~/Projects/Climate-AI-Hackathon-2023/data/organic_data.csv")
 
     # Load the materials into a Pandas dataframe
-    organic_data = pd.read_csv(directory)
+    organic_df = pd.read_csv(directory)
 
     # Create a Chroma client to store organic documents
     chroma_client = chromadb.Client(Settings(
@@ -29,7 +29,7 @@ def create_collection():
     metadatas = []
 
     # Iterate over the evidence to store it in the database
-    for record in evidence_df.to_dict(orient='records'):
+    for record in organic_df.to_dict(orient='records'):
         
         # Retrieve the important fields from the email record
         citation = record['citation']
