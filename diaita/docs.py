d7e3fae1b3a1c29221f45490d1ac839e81d57e0d
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 15:55:01 -0700

    changed func name

--- diaita/docs.py
+++ diaita/docs.py
@@ -2,7 +2,7 @@ from marvin import ai_fn
 
 from diaita.load import load_collection 
 
-def query_document(text, kind, n_results):
+def query_documents(text, kind, n_results):
     collection = load_collection()
     res = collection.query(query_texts=[text], where={"kind":kind}, n_results=n_results)
     ids = res['ids'][0]
