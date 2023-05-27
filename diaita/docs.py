237eb7dcbfae0b2c841919e4649508341e135477
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 16:41:23 -0700

    added metadata

--- diaita/docs.py
+++ diaita/docs.py
@@ -5,6 +5,6 @@ from diaita.load import load_collection
 def query_documents(text, kind, n_results):
     collection = load_collection()
     res = collection.query(query_texts=[text], where={"kind":kind}, n_results=n_results)
-    ids = res['ids'][0]
+    metadata = res['metadata'][0]
     docs = res['documents'][0]
-    return [(idx, doc) for idx, doc in zip(ids, docs)]
+    return [(datum['citation'], doc) for datum, doc in zip(metadata, docs)]
