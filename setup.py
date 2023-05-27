7cb759303d985423bb7069aaf49abba131b9652c
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 10:46:51 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='diaita',
+    version='0.1',
+    author='Campbell Hutcheson',
+    author_email='nchutcheson@gmail.com',
+    description='climate hackathon 2023',
+    packages=['diaita'],
+    install_requires=[
+        'langchain',
+        'marvin',
+    ],
+)
+
