d3b094071dfb0aaf74d131b70403cb54e9aad169
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 18:30:47 -0700

    added col

--- diaita/docs.py
+++ diaita/docs.py
@@ -3,7 +3,7 @@ from marvin import ai_fn
 from diaita.load import load_collection 
 
 def query_documents(text, col, kind, n_results):
-    res = collection.query(query_texts=[text], where={"kind":kind}, n_results=n_results)
+    res = col.query(query_texts=[text], where={"kind":kind}, n_results=n_results)
     metadata = res['metadatas'][0]
     docs = res['documents'][0]
     return [(datum['citation'], doc) for datum, doc in zip(metadata, docs)]
