e48527cb32b148d0dcc75177c55b7c4244a30d06
Campbell Hutcheson <nchutcheson@gmail.com>
2023-05-27 20:47:42 -0700

    improved prompt

--- diaita/prompts.py
+++ diaita/prompts.py
@@ -8,23 +8,23 @@ Documents: {documents}
 
 You have recieved a question from a company about USDA Organic compliance. 
 
-1. Break the question up into parts.
+Answer the question with the following steps:
 
-2. Identify any facts that the company provides about its situation.
+1. Identify the parts of the question and interpret them. Write out the parts and your interpretation.
 
-3. Brainstorm the reasons why the company might be asking this question.
+2. Identify any facts that the company provides about its situation and interpret them. Write out the facts and your interpretation.
 
-4. Brainstorm which parts of the documents might be relevant to the question.
+3. Brainstorm the reasons why the company might be asking this question. Write out your ideas.
 
-5. Brainstorm possible answers to the question, using the documents.
+4. Brainstorm which parts of the documents might be relevant to the question. Write out your ideas.
 
-Write out your ideas on each of these steps.
+5. Brainstorm possible answers to the question, using the documents. Write out your ideas.
 
 Then write out a response to the company's question.
 
-1. The response should include the context that you assumed for the question.
+1. The response should provide an answer to the question.
 
-2. The response should provide an answer to the question.
+2. The response should consider important questions that the company might want to consider.
 
 """
 
